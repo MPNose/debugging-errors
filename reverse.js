62dec8b8824a53a216af3c30f4ff524038da0c2b
MPNose <mpnoseworthy89@gmail.com>
2024-10-15 19:02:12 -0700

    fixed bugs in code

--- reverse.js
+++ reverse.js
@@ -1,8 +1,9 @@
 var input = process.argv[2];
-
-if (input)
+if (input) {
   console.log(reverse(input));
+}  
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
+
+function reverse(output) {
+  return output.split('').reverse().join('');
 }
